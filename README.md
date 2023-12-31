69d4386ded31009d8597a6507b8b6b664b55ff3b
CreeperGuy72192 <65190117+CreeperGuy72192@users.noreply.github.com>
2023-12-31 06:27:15 -0500

    ISOs are broken

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
-# ZoneOS X (the x doesn't mean anything, it was just the folder name of zoneweb)
+# ZoneOS
 
-ZoneOS Experiment in ASM, C, etc..
-This is probably a waste of time but I still want to try with real OSes -Creeper
-
-oh btw idk any asm or c (just like me with js, i barely know js)
-
-I'm using macOS Sonoma 14.2.1 (modified qemu build, macos bug causes some of the guest vm to go under the titlebar)
-
-run build.sh
+This is ZoneOS but on real hardware
--- boot/build.sh
+++ boot/build.sh
@@ -4,10 +4,10 @@
 nasm -f bin -o build/boot.bin boot.asm
 
 # Delete the previous ISO if it exists
-rm -f ../ISO/os.iso
+# rm -f ../ISO/os.iso
 
-# Create ISO image (macOS only)
-hdiutil makehybrid -o ../ISO/os.iso -hfs -iso -no-emul-boot -boot-load-size 4 -eltorito-boot build/boot.bin .
+# Create ISO image (macOS only) for the future, currently broken
+# hdiutil makehybrid -o ../ISO/os.iso -hfs -iso -no-emul-boot -boot-load-size 4 -eltorito-boot build/boot.bin .
 
 # Boot with the ISO using QEMU
-qemu-system-x86_64 -cdrom ../ISO/os.iso
+qemu-system-i386 -drive  file=build/boot.bin,format=raw -monitor stdio
