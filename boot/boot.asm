7a12b11a8916d1a1a68c14db4f07dfce2ed73aa3
CreeperGuy72192 <65190117+CreeperGuy72192@users.noreply.github.com>
2024-01-07 17:55:30 -0500

    aa

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,72 +1,64 @@
-[bits 16]
-[org 0x7c00]
+bits 16
+org 0x7c00
 
-start:
-    ; Set up the stack
-    mov ax, 0x7C0
-    add ax, 288
-    mov ss, ax
-    mov sp, 4096
+jmp main
 
-    ; Clear the screen
-    mov ah, 0x00 ; Video BIOS - Set Video Mode
-    mov al, 0x03 ; Video Mode 3 (80x25 text mode)
-    int 0x10 ; BIOS interrupt for video services
+Message db "Hello World!! THIS IS DOS!!!", 0x0
+AnyKey db "Press any key to Shutdown", 0x0
 
-    ; Load the GDT
-    lgdt [gdt_descriptor]
+Println:
+	lodsb
+	or al, al
+	jz complete
+	mov ah, 0x0e
+	int 0x10
+	jmp Println
+complete:
+	call PrintNwl
 
-    ; Enable protected mode
-    cli
-    mov eax, cr0
-    or eax, 0x1
-    mov cr0, eax
+PrintNwl:
+	mov al, 0
+	stosb
+	mov ah, 0x0E
+	mov al, 0x0D
+	int 0x10
+	mov al, 0x0A
+	int 0x10
+		ret
 
-    ; Jump to protected mode
-    jmp 0x08:init_protected_mode
+Shutdown:
+    mov ax, 0x1000
+    mov ax, ss
+    mov sp, 0xf000
+    mov ax, 0x5307
+    mov bx, 0x0001
+    mov cx, 0x0003
+    int 0x15
+	ret
+	
+Boot:
+    mov si, Message
+    call Println
+	mov si, AnyKey
+	call Println
+	call GetPressedKey
+	
+	call Shutdown
 
-[bits 32]
-init_protected_mode:
-    ; Set up data segment
-    mov ax, 0x10
-    mov ds, ax
-    mov ss, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
+GetPressedKey:
+	mov ah, 0
+	int 0x16
+	ret
 
-    ; Use the Multiboot header to get the kernel address
-    mov ebx, 0x8000 
-    mov eax, [ebx+8] ; Get the address of the module structure
-    mov ebx, [eax+4] ; Get the starting address of the module (kernel)
+main:
+	cli
+	mov ax,cs
+	mov ds,ax
+	mov es,ax
+	mov ss,ax
+	sti
 
-    ; Jump to the loaded kernel
-    jmp ebx ; Jump to the kernel's entry point
+	call Boot
 
-gdt_descriptor:
-    dw gdt_end - gdt_start - 1 ; Size of the GDT
-    dd gdt_start ; Address of the GDT
-
-gdt_start:
-    ; Null descriptor
-    dd 0
-    dd 0
-
-    ; Code segment descriptor
-    dw 0xFFFF ; Limit low (0-15)
-    dw 0 ; Base low (0-15)
-    db 0 ; Base middle (16-23)
-    db 10011010b ; Access byte: present, ring 0, code, readable
-    db 11001111b ; Granularity byte: 32-bit, 4K
-
-    ; Data segment descriptor
-    dw 0xFFFF ; Limit low (0-15)
-    dw 0 ; Base low (0-15)
-    db 0 ; Base middle (16-23)
-    db 10010010b ; Access byte: present, ring 0, data, expand-down
-    db 11001111b ; Granularity byte: 32-bit, 4K
-
-gdt_end:
-
-times 510 - ($ - $$) db 0 ; Pad the rest of the sector with zeros
-dw 0xAA55 ; Boot signature
+times 510 - ($-$$) db 0
+dw 0xAA55
